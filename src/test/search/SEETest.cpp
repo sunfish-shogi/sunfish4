9caa333e632b7e832b7e0500408f5946b5e4fbde
Kubo Ryosuke <sunfish.shogi@gmail.com>
2016-06-19 23:26:42 +0900

    Fix SEETest

--- src/test/search/SEETest.cpp
+++ src/test/search/SEETest.cpp
@@ -209,7 +209,7 @@ TEST(SEETest, testSortMoves) {
     moves.add(Move(Square::s74(), Square::s84(), false));
     moves.add(Move(Square::s85(), Square::s84(), false));
 
-    SEE::sortMoves(pos, moves, moves.begin(), false, false);
+    SEE::sortMoves(pos, moves, moves.begin(), false);
 
     ASSERT_EQ(5, moves.size());
     ASSERT_EQ(Move(Square::s74(), Square::s71(), true ), moves[0]);
@@ -240,44 +240,11 @@ TEST(SEETest, testSortMoves) {
     moves.add(Move(Square::s74(), Square::s84(), false));
     moves.add(Move(Square::s85(), Square::s84(), false));
 
-    SEE::sortMoves(pos, moves, moves.begin(), true, false);
+    SEE::sortMoves(pos, moves, moves.begin(), true);
 
     ASSERT_EQ(3, moves.size());
     ASSERT_EQ(Move(Square::s74(), Square::s71(), true ), moves[0]);
     ASSERT_EQ(Move(Square::s85(), Square::s84(), false), moves[1]);
     ASSERT_EQ(Move(Square::s46(), Square::s45(), false), moves[2]);
-  }
-
-  {
-    Position pos = PositionUtil::createPositionFromCsaString(
-      "P1 * -KE *  * -OU *  *  *  * \n"
-      "P2 *  *  *  *  *  *  *  *  * \n"
-      "P3 *  * -FU *  * -KY * +FU * \n"
-      "P4 *  * +FU *  *  *  *  *  * \n"
-      "P5 *  *  * +KE-KI-FU *  *  * \n"
-      "P6 *  *  *  * +KI+FU *  *  * \n"
-      "P7 *  *  *  *  *  *  *  *  * \n"
-      "P8 *  *  *  *  *  *  *  *  * \n"
-      "P9 *  *  *  * +OU *  *  *  * \n"
-      "P+\n"
-      "P-\n"
-      "+\n");
-    Moves moves;
-    moves.add(Move(Square::s23(), Square::s22(), true ));
-    moves.add(Move(Square::s46(), Square::s45(), false));
-    moves.add(Move(Square::s56(), Square::s45(), false));
-    moves.add(Move(Square::s56(), Square::s55(), false));
-    moves.add(Move(Square::s65(), Square::s53(), true ));
-    moves.add(Move(Square::s65(), Square::s73(), false));
-    moves.add(Move(Square::s65(), Square::s73(), true ));
-    moves.add(Move(Square::s74(), Square::s73(), true ));
-
-    SEE::sortMoves(pos, moves, moves.begin(), true, true);
-
-    ASSERT_EQ(4, moves.size());
-    ASSERT_EQ(Move(Square::s56(), Square::s55(), false), moves[0]);
-    ASSERT_EQ(Move(Square::s74(), Square::s73(), true ), moves[1]);
-    ASSERT_EQ(Move(Square::s23(), Square::s22(), true ), moves[2]);
-    ASSERT_EQ(Move(Square::s65(), Square::s73(), true ), moves[3]);
   }
 }
