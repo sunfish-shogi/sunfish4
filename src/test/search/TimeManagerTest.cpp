4ba9a7205426c8254a3a09ebc3f70ba503e73ebc
Kubo Ryosuke <sunfish.shogi@gmail.com>
2017-10-02 01:45:52 +0900

    Fix test

--- src/test/search/TimeManagerTest.cpp
+++ src/test/search/TimeManagerTest.cpp
@@ -12,10 +12,13 @@ using namespace sunfish;
 TEST(TimeManagerTest, testTimeManager) {
   Move move1(Square::s27(), Square::s26(), false);
   Move move2(Square::s83(), Square::s84(), false);
-  Move moves1[] = { move1, move2, move1, move2, move1, move2, move1, move2, move1, move2, };
-  Move moves2[] = { move2, move1, move2, move1, move2, move1, move2, move1, move2, move1, };
+  Move move3(Square::s17(), Square::s16(), true);
+  Move moves1[] = { move1, move2, move3, move1, move2, move3, move1, move2, move3, move1, };
+  Move moves2[] = { move2, move3, move1, move2, move3, move1, move2, move3, move1, move2, };
+  Move moves3[] = { move3, move1, move2, move3, move1, move2, move3, move1, move2, move3, };
   PV pv1(10, moves1);
   PV pv2(10, moves2);
+  PV pv3(10, moves3);
 
   auto inf = SearchConfig::InfinityTime;
   struct Scenario {
@@ -31,14 +34,14 @@ TEST(TimeManagerTest, testTimeManager) {
   Scenario scenario[] = {
     // clear optimumTimeMs maximumTimeMs elapsedMs depth score   pv shouldInterrupt
     {   true,          inf,          inf,    10000,   10,  100, pv1,          false },
-    {  false,          inf,          inf,   581000,   11,  100, pv1,          false },
-    {  false,          inf,          inf,   582000,   12,  100, pv2,          false }, // 97% of maximumTimeMs
+    {  false,          inf,          inf,   581000,   11,  100, pv2,          false },
+    {  false,          inf,          inf,   582000,   12,  100, pv3,          false },
     {   true,          inf,       600000,    10000,   10,  100, pv1,          false },
-    {  false,          inf,       600000,   581000,   11,  100, pv1,          false },
-    {  false,          inf,       600000,   582000,   12,  100, pv2,           true }, // 97% of maximumTimeMs
+    {  false,          inf,       600000,   479000,   11,  100, pv2,          false },
+    {  false,          inf,       600000,   480000,   12,  100, pv3,           true }, // 80% of maximumTimeMs
     {   true,       100000,       600000,    10000,   10,  100, pv1,          false },
-    {  false,       100000,       600000,   581000,   11,  100, pv2,          false },
-    {  false,       100000,       600000,   582000,   12,  100, pv2,           true }, // 97% of maximumTimeMs
+    {  false,       100000,       600000,   479000,   11,  100, pv2,          false },
+    {  false,       100000,       600000,   480000,   12,  100, pv3,           true }, // 80% of maximumTimeMs
   };
 
   TimeManager timeManager;
