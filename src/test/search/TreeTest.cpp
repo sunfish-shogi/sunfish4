a70b6f450cb0a7fc00d669ce17ec3a8590a87e28
Kubo Ryosuke <sunfish-shogi@users.noreply.github.com>
2021-09-18 15:01:32 +0900

    Update test

--- src/test/search/TreeTest.cpp
+++ src/test/search/TreeTest.cpp
@@ -183,32 +183,32 @@ TEST(TreeTest, testAddKiller) {
 
   addKiller(tree, move1);
   ASSERT_EQ(move1, tree.nodes[5].killerMove1);
-  ASSERT_EQ(    0, tree.nodes[5].killerCount1);
+  ASSERT_EQ(    4, tree.nodes[5].killerCount1);
   ASSERT_EQ(Move::none(), tree.nodes[5].killerMove2);
 
   addKiller(tree, move2);
   ASSERT_EQ(move2, tree.nodes[5].killerMove1);
-  ASSERT_EQ(    0, tree.nodes[5].killerCount1);
+  ASSERT_EQ(    4, tree.nodes[5].killerCount1);
   ASSERT_EQ(move1, tree.nodes[5].killerMove2);
-  ASSERT_EQ(   -1, tree.nodes[5].killerCount2);
+  ASSERT_EQ(    3, tree.nodes[5].killerCount2);
 
   addKiller(tree, move3);
-  ASSERT_EQ(move3, tree.nodes[5].killerMove1);
-  ASSERT_EQ(    0, tree.nodes[5].killerCount1);
-  ASSERT_EQ(move2, tree.nodes[5].killerMove2);
-  ASSERT_EQ(   -1, tree.nodes[5].killerCount2);
+  ASSERT_EQ(move2, tree.nodes[5].killerMove1);
+  ASSERT_EQ(    4, tree.nodes[5].killerCount1);
+  ASSERT_EQ(move1, tree.nodes[5].killerMove2);
+  ASSERT_EQ(    3, tree.nodes[5].killerCount2);
 
   addKiller(tree, move2);
   ASSERT_EQ(move2, tree.nodes[5].killerMove1);
-  ASSERT_EQ(    1, tree.nodes[5].killerCount1);
-  ASSERT_EQ(move3, tree.nodes[5].killerMove2);
-  ASSERT_EQ(   -1, tree.nodes[5].killerCount2);
+  ASSERT_EQ(    5, tree.nodes[5].killerCount1);
+  ASSERT_EQ(move1, tree.nodes[5].killerMove2);
+  ASSERT_EQ(    2, tree.nodes[5].killerCount2);
 
   addKiller(tree, move2);
   ASSERT_EQ(move2, tree.nodes[5].killerMove1);
-  ASSERT_EQ(    3, tree.nodes[5].killerCount1);
-  ASSERT_EQ(move3, tree.nodes[5].killerMove2);
-  ASSERT_EQ(   -2, tree.nodes[5].killerCount2);
+  ASSERT_EQ(    6, tree.nodes[5].killerCount1);
+  ASSERT_EQ(move1, tree.nodes[5].killerMove2);
+  ASSERT_EQ(    1, tree.nodes[5].killerCount2);
 }
 
 TEST(TreeTest, testDoMove) {
